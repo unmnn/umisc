6fef1de98da519298f30a0931e1f4f2014b814de
unmnn <uli.niemann@ovgu.de>
2020-12-02 17:49:40 +0100

    capitalize_prefix()

--- R/misc.R
+++ R/misc.R
@@ -18,3 +18,32 @@ mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
+
+
+#' Capitalizes all letters in a character string until a separator character
+#'
+#' Returns original character string if it does not contain the separator
+#' character.
+#'
+#' @param char A character string.
+#' @param sep The separator character.
+#'
+#' @return The modified character string.
+#' @export
+#'
+#' @examples
+#' char <- "adsl_depression"
+#' capitalize_prefix(char)
+capitalize_prefix <- function(char, sep = "_") {
+  s <- strsplit(char, "_")
+  vapply(s, function(x){
+    if(length(x) == 1) {
+      x
+    } else {
+      paste0(toupper(x[1]), "_", x[2])
+    }
+  },
+  FUN.VALUE = character(1),
+  USE.NAMES = FALSE
+  )
+}
